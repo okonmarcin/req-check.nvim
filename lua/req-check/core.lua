5944627b90108bfc07909eae0eeba772fc37e72e
okonmarcin <marcin.okon@flyrlabs.com>
2024-01-13 11:03:38 +0100

    Inform user if all deps are up to date

--- lua/req-check/core.lua
+++ lua/req-check/core.lua
@@ -23,27 +23,31 @@ function M.check_requirement()
 	if outdated_libs then
 		outdated_libs:read("*line")
 		outdated_libs:read("*line")
-	end
-	for lib_row in outdated_libs:lines() do
-		local splited_values = {}
-		for w in string.gmatch(lib_row, "%S+") do
-			table.insert(splited_values, w)
+		for lib_row in outdated_libs:lines() do
+			local splited_values = {}
+			for w in string.gmatch(lib_row, "%S+") do
+				table.insert(splited_values, w)
+			end
+			if lib_mapping[splited_values[1]] ~= nil then
+				table.insert(lib_mapping[splited_values[1]], splited_values[2])
+				table.insert(lib_mapping[splited_values[1]], splited_values[3])
+			end
 		end
-		if lib_mapping[splited_values[1]] ~= nil then
-			table.insert(lib_mapping[splited_values[1]], splited_values[2])
-			table.insert(lib_mapping[splited_values[1]], splited_values[3])
-		end
 	end
-
 	local namespace = vim.api.nvim_create_namespace("req_check")
+	local all_up_to_date = true
 	for _, lib_info in pairs(lib_mapping) do
 		if #lib_info == 3 then
+			all_up_to_date = false
 			vim.api.nvim_buf_set_extmark(buf, namespace, lib_info[1] - 1, 0, {
 				virt_text = { { "# " .. lib_info[2] .. " ===> " .. lib_info[3], "WarningMsg" } },
 				virt_text_pos = "eol",
 				hl_group = "WarningMsg"
 			})
 		end
+	end
+	if all_up_to_date == true then
+		vim.api.nvim_out_write("All dependecies are up to date.\n")
 	end
 end
 
