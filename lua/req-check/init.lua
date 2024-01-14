480c9389ddcc74b4d39fdbc638d2ff2fab647417
okonmarcin <marcin.okon@flyrlabs.com>
2024-01-14 18:58:33 +0100

    Separate commands for updating and installation

--- lua/req-check/init.lua
+++ lua/req-check/init.lua
@@ -9,6 +9,9 @@ function M.setup()
 
 	vim.api.nvim_create_user_command("ReqUpdate", function()
 		core.update_requirements()
+	end, { nargs = 0 })
+
+	vim.api.nvim.nvim_create_user_command("ReqInstall", function()
 		core.reinstall_requirements()
 	end, { nargs = 0 })
 end
